7c2fe4a11d6b742d91100f09fed47afb67876998
kutay malik tatas <tatasmalik@gmail.com>
2024-11-12 10:40:26 +0300

    - Added .env

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
-// src/services/api.js
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://localhost:7262/BasicApp/api/v1",
+    baseURL: process.env.REACT_APP_API_URL,
     headers: {
         "Content-Type": "application/json",
     },
